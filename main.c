57c1566e6d0ff37ad6cf6b7d923e27b586ea2c81
Guilherme Andrade e Chaves <83043492+guiaech@users.noreply.github.com>
2021-11-16 02:25:34 +0000

    nível de dificuldade

--- main.c
+++ main.c
@@ -19,9 +19,33 @@ int main()
     int ganhou = 0;
     int tentativas = 1;
     double pontos = 1000;
+    int acertou = 0;
+
+    int nivel;
+    printf("Qual nível de dificuldade quer jogar? \n");
+    printf("(1) Fácil  (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int numerodetentativas ;
+
+    switch(nivel)
+    {
+      case 1:
+      numerodetentativas = 20;
+      break;
+
+      case 2:
+      numerodetentativas = 15;
+      break;
+
+      default:
+      numerodetentativas = 6;
+      break;
+    }
 
 
-    while (ganhou == 0)
+    for (int i = 1; i<= numerodetentativas; i++ )
     {
         printf("\n");
         printf("Tentativa %d \n",tentativas);
@@ -35,14 +59,11 @@ int main()
           continue;
         }
 
-        int acertou = (chute == numerosecreto);
+        acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
 
-        if (acertou)
-        {
-            printf("Parabéns você acertou! \n");
-            printf("Jogue de novo, você é um bom jogador! \n");
-            ganhou = 1;
+        if (acertou){
+        break;
         }
         else if (maior)
           {
@@ -57,9 +78,19 @@ int main()
           double pontosperdidos = abs(chute - numerosecreto) / (double)2;
           pontos = pontos - pontosperdidos;
     }
-  printf("\n");
-  printf("********* Fim de jogo! **********\n");
-  printf("  Você acertou em %d tentativa(s)\n", tentativas-1);
-  printf("   Sua pontuação foi de %.1f!\n", pontos);
-  printf("*********************************\n");
+    if (acertou){
+      printf("Parabéns você acertou! \n");
+      printf("Jogue de novo, você é um bom jogador! \n");
+      printf("\n");
+      printf("********* Fim de jogo! **********\n");
+      printf("  Você acertou em %d tentativa(s)\n", tentativas-1);
+      printf("   Sua pontuação foi de %.1f!\n", pontos);
+      printf("*********************************\n");
+    }else
+    {
+      printf("********* Fim de jogo! **********\n");
+      printf("  Você perdeu, tente novamente   \n");
+      printf("*********************************\n");
+    }
+
 }
